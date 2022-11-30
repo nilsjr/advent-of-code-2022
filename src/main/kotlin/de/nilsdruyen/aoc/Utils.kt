838002b2b3536584e939ce10bb4dc7ef6c345be8
Nils Druyen <nils.druyen@fressnapf.com>
2022-11-30 19:15:44 +0100

    Update template changes

--- src/main/kotlin/de/nilsdruyen/aoc/Utils.kt
+++ src/main/kotlin/de/nilsdruyen/aoc/Utils.kt
@@ -7,9 +7,12 @@ import java.security.MessageDigest
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src/main/resources", "$name.txt").readLines()
+fun readInput(name: String) = File("src/main/resources", "$name.txt")
+    .readLines()
 
 /**
- * Converts string to de.nilsdruyen.de.nilsdruyen.aoc.md5 hash.
+ * Converts string to md5 hash.
  */
-fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
+    .toString(16)
+    .padStart(32, '0')
