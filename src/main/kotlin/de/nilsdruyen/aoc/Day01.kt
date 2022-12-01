c060f8bcf2146cebc16b3b6440ee31b584832211
Nils Druyen <nils.druyen@fressnapf.com>
2022-12-01 14:25:35 +0100

    Solve Day01.kt

--- src/main/kotlin/de/nilsdruyen/aoc/Day01.kt
+++ src/main/kotlin/de/nilsdruyen/aoc/Day01.kt
@@ -2,16 +2,42 @@ package de.nilsdruyen.aoc
 
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var elveIndex = 0
+        return input
+            .mapNotNull {
+                if (it.isBlank()) {
+                    elveIndex++
+                    null
+                } else {
+                    elveIndex to it
+                }
+            }
+            .groupBy { it.first }
+            .map { it.value.sumOf { it.second.toInt() } }
+            .max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var elveIndex = 0
+        return input
+            .mapNotNull {
+                if (it.isBlank()) {
+                    elveIndex++
+                    null
+                } else {
+                    elveIndex to it
+                }
+            }
+            .groupBy { it.first }
+            .map { it.value.sumOf { it.second.toInt() } }
+            .sorted()
+            .takeLast(3)
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
