b66ea0b9d9d9822096cee344ae1eef201639282d
Nils Druyen <nils.druyen@fressnapf.com>
2022-12-01 20:32:24 +0100

    Optimize code

--- src/main/kotlin/de/nilsdruyen/aoc/Day01.kt
+++ src/main/kotlin/de/nilsdruyen/aoc/Day01.kt
@@ -1,39 +1,24 @@
 package de.nilsdruyen.aoc
 
 fun main() {
-    fun part1(input: List<String>): Int {
+
+    fun List<String>.groupByCalories(): List<Int> {
         var elveIndex = 0
-        return input
-            .mapNotNull {
-                if (it.isBlank()) {
-                    elveIndex++
-                    null
-                } else {
-                    elveIndex to it
-                }
+        return mapNotNull {
+            if (it.isBlank()) {
+                elveIndex++
+                null
+            } else {
+                elveIndex to it
             }
-            .groupBy { it.first }
-            .map { it.value.sumOf { it.second.toInt() } }
-            .max()
+        }.groupBy { it.first }.map {
+            it.value.sumOf { foodCalories -> foodCalories.second.toInt() }
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        var elveIndex = 0
-        return input
-            .mapNotNull {
-                if (it.isBlank()) {
-                    elveIndex++
-                    null
-                } else {
-                    elveIndex to it
-                }
-            }
-            .groupBy { it.first }
-            .map { it.value.sumOf { it.second.toInt() } }
-            .sorted()
-            .takeLast(3)
-            .sum()
-    }
+    fun part1(input: List<String>): Int = input.groupByCalories().max()
+
+    fun part2(input: List<String>): Int = input.groupByCalories().sorted().takeLast(3).sum()
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
