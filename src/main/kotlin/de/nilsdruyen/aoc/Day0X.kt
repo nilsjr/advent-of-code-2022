988d2c0f3ae51effc75b3eea25fc51245aa7d1e4
Nils Druyen <nils.druyen@fressnapf.com>
2022-12-02 09:03:48 +0100

    Add Day0X.kt template

---
+++ src/main/kotlin/de/nilsdruyen/aoc/Day0X.kt
@@ -0,0 +1,20 @@
+package de.nilsdruyen.aoc
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day01_test")
+    check(part1(testInput) == 24000)
+
+    val input = readInput("Day01")
+    println(part1(input))
+    println(part2(input))
+}
