ec9ba0fbf7c7dcb839aaca4421e92571334a031c
Nils Druyen <nils.druyen@fressnapf.com>
2022-12-03 12:55:31 +0100

    Add puzzle files

--- src/main/kotlin/de/nilsdruyen/aoc/Day03.kt
+++ src/main/kotlin/de/nilsdruyen/aoc/Day03.kt
@@ -2,17 +2,6 @@ package de.nilsdruyen.aoc
 
 fun main() {
 
-    fun Char.prio(): Int = if (code > 96) code - 96 else code - 38
-
-    fun List<String>.sumUpPrio(): Int = map {
-        val center = it.length / 2
-        it.slice(0 until center) to it.slice(center until it.length)
-    }
-        .map { parts ->
-            parts.first.first { parts.second.contains(it) }
-        }
-        .sumOf(Char::prio)
-
     fun part1(input: List<String>): Int = input.sumUpPrio()
 
     fun part2(input: List<String>): Int {
@@ -32,3 +21,13 @@ fun main() {
     println(part2(input))
 }
 
+fun Char.prio(): Int = if (code > 96) code - 96 else code - 38
+
+fun List<String>.sumUpPrio(): Int = map {
+    val center = it.length / 2
+    it.slice(0 until center) to it.slice(center until it.length)
+}
+    .map { parts ->
+        parts.first.first { parts.second.contains(it) }
+    }
+    .sumOf(Char::prio)
